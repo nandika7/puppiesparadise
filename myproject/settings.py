3ab75c2ea6ce4556f0a8fdf6b0f232cf9f499373
nandika7 <95585293+nandika7@users.noreply.github.com>
2022-07-24 22:36:49 -0700

    Add files via upload

--- myproject/settings.py
+++ myproject/settings.py
@@ -42,10 +42,10 @@ INSTALLED_APPS = [
 ]
 
 STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR,'static')
+#]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
